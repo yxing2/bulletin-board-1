25e0c932c2e64b8d5aa5664095b02412ac085669
yxing2 <136658270+yxing2@users.noreply.github.com>
2023-08-14 21:42:50 +0000

    board created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Board resource:
+
+  # CREATE
+  post("/insert_board", { :controller => "boards", :action => "create" })
+          
+  # READ
+  get("/boards", { :controller => "boards", :action => "index" })
+  
+  get("/boards/:path_id", { :controller => "boards", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_board/:path_id", { :controller => "boards", :action => "update" })
+  
+  # DELETE
+  get("/delete_board/:path_id", { :controller => "boards", :action => "destroy" })
+
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
